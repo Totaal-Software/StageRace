440c452eed3792de55e32a934f4a706d8ed659b3
Sander Verhagen <sander@sanderverhagen.net>
2006-08-21 18:11:23 +0000

    2005, 3, 13, 1885
    git-svn-id: https://svn.totaalsoftware.com/svn/StageRace/trunk@43 60b0b350-b89f-7443-af7d-bf35776c3b91

--- StageRace_p.C
+++ StageRace_p.C
@@ -2,7 +2,7 @@
 
 
 /* File created by MIDL compiler version 5.01.0164 */
-/* at Sun Aug 20 00:42:03 2006
+/* at Sun Aug 20 13:29:59 2006
  */
 /* Compiler settings for D:\Projects\StageRace\trunk\StageRace.idl:
     Oicf (OptLev=i2), W1, Zp8, env=Win32, ms_ext, c_ext
